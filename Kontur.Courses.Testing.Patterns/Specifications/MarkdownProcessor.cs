b845605aee818cb334c905cf34a03005ba01e75f
Sergey <serwaas@yandex.ru>
2015-05-01 20:29:11 +0300

    Добавлены тесты

--- Kontur.Courses.Testing.Patterns/Specifications/MarkdownProcessor.cs
+++ Kontur.Courses.Testing.Patterns/Specifications/MarkdownProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Kontur.Courses.Testing.Patterns.Specifications
 {
@@ -8,7 +10,18 @@ namespace Kontur.Courses.Testing.Patterns.Specifications
 	{
 		public string Render(string input)
 		{
-			throw new NotImplementedException();
+            var emReplacer = new Regex(@"([^\w\\]|^)_(.*?[^\\])_(\W|$)");
+            var strongReplacer = new Regex(@"([^\w\\]|^)__(.*?[^\\])__(\W|$)");
+            input = strongReplacer.Replace(input,
+                    match => match.Groups[1].Value +
+                            "<strong>" + match.Groups[2].Value + "</strong>" +
+                            match.Groups[3].Value);
+            input = emReplacer.Replace(input,
+                    match => match.Groups[1].Value +
+                            "<em>" + match.Groups[2].Value + "</em>" +
+                            match.Groups[3].Value);
+            input = input.Replace(@"\_", "_");
+            return input;
 		}
 	}
 
@@ -17,7 +30,52 @@ namespace Kontur.Courses.Testing.Patterns.Specifications
 	{
 		private readonly MarkdownProcessor md = new MarkdownProcessor();
 
-		//TODO see Markdown.txt
+	    [Test]
+	    public void without_underscore()
+	    {
+	        var str = "without underscore";
+            var res = new MarkdownProcessor();
+
+            Assert.AreEqual(str,res.Render(str));
+	    }
+
+
+        [TestCase("_a_", Result = @"<em>a</em>", TestName = "just em")]
+        [TestCase("_a", Result = @"_a", TestName = "unpaired underscore")]
+        [TestCase("_a_ b", Result = @"<em>a</em> b", TestName = "em and string")]
+        [TestCase("a _b_", Result = @"a <em>b</em>", TestName = "string and em")]
+        [TestCase("a_a_a", Result = @"a_a_a", TestName = "underscore in string")]
+        [TestCase("_a_a_", Result = @"<em>a_a</em>", TestName = "em and underscore in string")]
+        [TestCase("_a__", Result = @"<em>a_</em>", TestName = " underscore string and 2 underscores")]
+
+        public string tag_em(string input)
+        {
+            var res = new MarkdownProcessor();
+            return res.Render(input);
+        }
+
+        [TestCase("__a__", Result = @"<strong>a</strong>", TestName = "just strong")]
+        [TestCase("__a", Result = @"__a", TestName = "unpaired underscores")]
+        [TestCase("__a_", Result = @"<em>_a</em>", TestName = "2 underscores string and underscore")]
+        [TestCase("__a__ b", Result = @"<strong>a</strong> b", TestName = "strong and string")]
+        [TestCase("a __b__", Result = @"a <strong>b</strong>", TestName = "string and strong")]
+        [TestCase("a__a__a", Result = @"a__a__a", TestName = "underscore in string")]
+        [TestCase("__a__a__", Result = @"<strong>a__a</strong>", TestName = "strong and 2 underscores in string")]
+        public string tag_strong(string input)
+        {
+            var res = new MarkdownProcessor();
+            return res.Render(input);
+        }
+
+        [TestCase(@"\__a\__", Result = @"_<em>a</em>_", TestName = "tag em with 2 ecrans")]
+        [TestCase(@"\__a_", Result = @"_<em>a</em>", TestName = "tag em with 2 ecrans")]
+        [TestCase(@"_a _\_b__", Result = @"<em>a __b_</em>", TestName = "underscore and strong")]
+        public string ecran(string input)
+        {
+            var res = new MarkdownProcessor();
+            return res.Render(input);
+        }
+        //TODO see Markdown.txt
 		
 	}
 }
--- Kontur.Courses.Testing/WordsStatistics_Tests.cs
+++ Kontur.Courses.Testing/WordsStatistics_Tests.cs
@@ -1,48 +1,97 @@
 ﻿using System;
+using System.Collections;
+using System.Threading;
 using Kontur.Courses.Testing.Implementations;
 using NUnit.Framework;
 
 namespace Kontur.Courses.Testing
 {
-	public class WordsStatistics_Tests
-	{
-		public Func<IWordsStatistics> createStat = () => new WordsStatistics_CorrectImplementation(); // меняется на разные реализации при запуске exe
-		public IWordsStatistics stat;
+    public class WordsStatistics_Tests
+    {
+        public Func<IWordsStatistics> createStat = () => new WordsStatistics_CorrectImplementation(); // меняется на разные реализации при запуске exe
+        public IWordsStatistics stat;
 
-		[SetUp]
-		public void SetUp()
-		{
-			stat = createStat();
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            stat = createStat();
+        }
 
-		[Test]
-		public void no_stats_if_no_words()
-		{
-			CollectionAssert.IsEmpty(stat.GetStatistics());
-		}
+        [Test]
+        public void no_stats_if_no_words()
+        {
+            CollectionAssert.IsEmpty(stat.GetStatistics());
+        }
 
-		[Test]
-		public void same_word_twice()
-		{
-			stat.AddWord("xxx");
-			stat.AddWord("xxx");
-			CollectionAssert.AreEqual(new[] { Tuple.Create(2, "xxx") }, stat.GetStatistics());
-		}
 
-		[Test]
-		public void single_word()
-		{
-			stat.AddWord("hello");
-			CollectionAssert.AreEqual(new[] { Tuple.Create(1, "hello") }, stat.GetStatistics());
-		}
+        [Test]
+        public void word_length_more_than_ten()
+        {
+            stat.AddWord("aAaAbBasdsddsad");
+            CollectionAssert.AreEqual(new[] { Tuple.Create(1, "aaaabbasds") }, stat.GetStatistics());
+        }
+        [Test]
+        public void test_frequency()
+        {
+            stat.AddWord("hello");
+            stat.AddWord("kontur");
+            stat.AddWord("hello");
+            stat.AddWord("ololo");
+            stat.AddWord("lol");
+            
+            stat.AddWord("kontur");
+            stat.AddWord("Kontur");
+            stat.AddWord("kontur");
+            CollectionAssert.AreEqual(new[] { Tuple.Create(4, "kontur"), 
+                Tuple.Create(2, "hello"),Tuple.Create(1, "lol"),
+                Tuple.Create(1, "ololo") }, stat.GetStatistics());
+        }
 
-		[Test]
-		public void two_same_words_one_other()
-		{
-			stat.AddWord("hello");
-			stat.AddWord("world");
-			stat.AddWord("world");
-			CollectionAssert.AreEqual(new[] { Tuple.Create(2, "world"), Tuple.Create(1, "hello") }, stat.GetStatistics());
-		}
-	}
+        [Test]
+        public void words_with_len_11()
+        {
+            
+                stat.AddWord("AaaaaaaaaaB");
+                stat.AddWord("aaaaaaaaaaC");
+            
+            CollectionAssert.AreEqual(new[] { Tuple.Create(2, "aaaaaaaaaa") }, stat.GetStatistics());
+        }
+
+        [Test]
+        public void word_with_zero_len()
+        {
+            stat.AddWord("Hell");
+
+            stat.AddWord("");
+
+            CollectionAssert.AreEqual(new[] { Tuple.Create(1, "hell") }, stat.GetStatistics());
+        }
+        [Test,Timeout(2000)]
+        public void count_of_words_equal_20000()
+        {
+            ArrayList res = new ArrayList();
+            for (int i = 0; i < 20000; i++)
+            {
+                stat.AddWord(i.ToString());
+                // res[i] = Tuple.Create(1, (""+i));
+                res.Add(Tuple.Create(1, i.ToString()));
+            }
+            res.Sort();
+
+
+            CollectionAssert.AreEqual(res, stat.GetStatistics());
+        }
+        [Test]
+        public void input_null_string()
+        {
+
+
+            stat.AddWord("Hell");
+
+            stat.AddWord(null);
+
+            CollectionAssert.AreEqual(new[] { Tuple.Create(1, "hell") }, stat.GetStatistics());
+        }
+        
+    }
 }
